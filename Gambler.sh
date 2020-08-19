29aaf0d9a17c330cb0d7eaa831dcd4bf26df0ad0
soumen-gif <soumentaki1996@gmail.com>
2020-08-19 21:39:45 +0530

    win or loss

--- Gambler.sh
+++ Gambler.sh
@@ -1,15 +1,25 @@
-
 #!/bin/bash -x
 
 stakes=100
 day=0
 game=0
+win=1
+lose=0
+x=$((RANDOM%2))
 function gamble(){
-        while [[ $game -lt 1 ]]
-        do
+    while [[ $game -lt 1 ]]
+      do 
+        if [[ $x -eq $win ]]
+            then
+               echo "gambler won the game"
                 stakes=$(($stakes+1))
-                ((game++))
-        done
+         else 
+              echo "gambler lost the game"
+              stakes=$(($stakes+1))
+      fi
+  done
+ }
 
-   }
+
+
 
