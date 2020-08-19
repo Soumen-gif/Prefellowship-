4fe9ddff2549607e2c0ba69564574fac0818fef1
soumen-gif <soumentaki1996@gmail.com>
2020-08-19 21:55:20 +0530

    win or loss until the day

---
+++ Gambler1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -x
+
+stakes=100
+day=0
+game=0
+win=1
+lose=0
+winningTarget=150
+loosingTarget=50
+
+function gamble(){
+   while [[ $stakes -gt $loosingTarget && $stakes -lt $winningTarget ]]
+   do
+      if [[ $((RANDOM%2)) -eq $win ]]
+      then
+         stakes=$(($stakes+1))
+      else
+         stakes=$(($stakes-1))
+      fi
+         ((game++))
+   done
+}
+
